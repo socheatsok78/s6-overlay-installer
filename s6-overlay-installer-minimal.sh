64f841491612977d80f5e61b58c60cc3deab9077
Socheat Sok <alex@socheat.net>
2023-06-25 12:31:09 +0700

    Fix installer

--- s6-overlay-installer-minimal.sh
+++ s6-overlay-installer-minimal.sh
@@ -2,18 +2,20 @@
 
 S6_TEMP=$(mktemp -d)
 S6_ARCH=$(uname -m)
+
 S6_OVERLAY_VERSION="$1"
+shift
 
 s6-install () {
-  echo "Downloading $1 archive..."
-  curl -sLO https://github.com/just-containers/s6-overlay/releases/download/v${S6_OVERLAY_VERSION}/$1
-  curl -sLO https://github.com/just-containers/s6-overlay/releases/download/v${S6_OVERLAY_VERSION}/$1.sha256
-  
-  echo "Verify $1 checksums..."
-  sha256sum -c $1.sha256
+	echo "Downloading $1 archive..."
+	curl -sLO "https://github.com/just-containers/s6-overlay/releases/download/v${S6_OVERLAY_VERSION}/$1"
+	curl -sLO "https://github.com/just-containers/s6-overlay/releases/download/v${S6_OVERLAY_VERSION}/$1.sha256"
 
-  echo "Installing $1..."
-  tar -C / -Jxpf "$1"
+	echo "Verify $1 checksums..."
+	sha256sum -c $1.sha256
+
+	echo "Installing $1..."
+	tar -C / -Jxpf "$1"
 }
 
 cd "${S6_TEMP}"
