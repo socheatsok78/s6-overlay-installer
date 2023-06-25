e8235b7a3c292b4f54bb9f5039d035f66dd7914e
Socheat Sok <alex@socheat.net>
2023-06-25 13:30:01 +0700

    Improve installer

--- s6-overlay-installer-minimal.sh
+++ s6-overlay-installer-minimal.sh
@@ -1,32 +1,36 @@
 #!/bin/sh
 set -e
 
+S6_DOWNLOAD_URL=https://github.com/just-containers/s6-overlay/releases/download
 S6_TEMP=$(mktemp -d)
 S6_ARCH=$(uname -m)
-
-S6_OVERLAY_VERSION="${S6_OVERLAY_VERSION:1}"
+S6_OVERLAY_VERSION="${1:-$S6_OVERLAY_VERSION}"
 
 if [[ -z "${S6_OVERLAY_VERSION}" ]]; then
 	echo "[Error] S6_OVERLAY_VERSION is not set."
 	exit 1
 fi
 
-
 s6_install () {
-	echo "Downloading $1 archive..."
-	curl -sLO "https://github.com/just-containers/s6-overlay/releases/download/v${S6_OVERLAY_VERSION}/$1"
-	curl -sLO "https://github.com/just-containers/s6-overlay/releases/download/v${S6_OVERLAY_VERSION}/$1.sha256"
+	echo "Downloading $1 archive from $S6_DOWNLOAD_URL/v${S6_OVERLAY_VERSION}/$1..."
+	curl -fSLO "$S6_DOWNLOAD_URL/v${S6_OVERLAY_VERSION}/$1"
+	echo
 
 	echo "Verify $1 checksums..."
+	curl -fSLO "$S6_DOWNLOAD_URL/v${S6_OVERLAY_VERSION}/$1.sha256"
 	sha256sum -c "$1.sha256"
+	echo
 
 	echo "Installing $1..."
 	tar -C / -Jxpf "$1"
 }
 
-cd "${S6_TEMP}"
-s6_install "s6-overlay-noarch.tar.xz"
-s6_install "s6-overlay-${S6_ARCH}.tar.xz"
+cd "${S6_TEMP}" && {
+	s6_install "s6-overlay-noarch.tar.xz"
+	s6_install "s6-overlay-${S6_ARCH}.tar.xz"
+
+}
 
 echo "Cleaning up..."
+cd -
 rm -rf "${S6_TEMP}"
