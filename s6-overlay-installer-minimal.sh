8b80b013ef04af4f83c090baa08b5548bac8d38e
Socheat Sok <alex@socheat.net>
2023-06-25 12:37:51 +0700

    Fix function name 2

--- s6-overlay-installer-minimal.sh
+++ s6-overlay-installer-minimal.sh
@@ -5,7 +5,7 @@ S6_ARCH=$(uname -m)
 
 S6_OVERLAY_VERSION="$1"
 
-sx-install () {
+s6_install () {
 	echo "Downloading $1 archive..."
 	curl -sLO "https://github.com/just-containers/s6-overlay/releases/download/v${S6_OVERLAY_VERSION}/$1"
 	curl -sLO "https://github.com/just-containers/s6-overlay/releases/download/v${S6_OVERLAY_VERSION}/$1.sha256"
@@ -18,8 +18,8 @@ sx-install () {
 }
 
 cd "${S6_TEMP}"
-sx-install "s6-overlay-noarch.tar.xz"
-sx-install "s6-overlay-${S6_ARCH}.tar.xz"
+s6_install "s6-overlay-noarch.tar.xz"
+s6_install "s6-overlay-${S6_ARCH}.tar.xz"
 
 echo "Cleaning up..."
 rm -rf "${S6_TEMP}"
