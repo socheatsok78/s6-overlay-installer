67ecfc94af42f5a695d69ad20d621d951dd59442
Socheat Sok <alex@socheat.net>
2023-06-25 12:45:13 +0700

    Add check for S6_OVERLAY_VERSION

--- s6-overlay-installer-minimal.sh
+++ s6-overlay-installer-minimal.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
+set -e
 
 S6_TEMP=$(mktemp -d)
 S6_ARCH=$(uname -m)
 
-S6_OVERLAY_VERSION="$1"
+S6_OVERLAY_VERSION="${S6_OVERLAY_VERSION:1}"
+
+if [[ -z "${S6_OVERLAY_VERSION}" ]]; then
+	echo "[Error] S6_OVERLAY_VERSION is not set."
+	exit 1
+fi
+
 
 s6_install () {
 	echo "Downloading $1 archive..."
@@ -11,7 +18,7 @@ s6_install () {
 	curl -sLO "https://github.com/just-containers/s6-overlay/releases/download/v${S6_OVERLAY_VERSION}/$1.sha256"
 
 	echo "Verify $1 checksums..."
-	sha256sum -c $1.sha256
+	sha256sum -c "$1.sha256"
 
 	echo "Installing $1..."
 	tar -C / -Jxpf "$1"
