178a832cb850b4007ada48ff9a8105e9741de12a
Socheat Sok <socheatsok78@gmail.com>
2023-06-26 23:37:02 +0700

    Update installer logs

--- s6-overlay-installer-syslogd.sh
+++ s6-overlay-installer-syslogd.sh
@@ -2,8 +2,6 @@
 set -e
 
 S6_DOWNLOAD_URL=https://github.com/just-containers/s6-overlay/releases/download
-S6_TEMP=$(mktemp -d)
-S6_ARCH=$(uname -m)
 S6_OVERLAY_VERSION="${1:-$S6_OVERLAY_VERSION}"
 
 if [ -z "${S6_OVERLAY_VERSION}" ]; then
@@ -16,14 +14,20 @@ s6_install () {
 	curl -fSLO "$S6_DOWNLOAD_URL/${S6_OVERLAY_VERSION}/$1"
 	echo
 
-	echo "Verify $1 checksums..."
-	curl -fSLO "$S6_DOWNLOAD_URL/${S6_OVERLAY_VERSION}/$1.sha256"
+	echo "  - Verify $1 checksums..."
+	curl -fsSLO "$S6_DOWNLOAD_URL/${S6_OVERLAY_VERSION}/$1.sha256"
 	sha256sum -c "$1.sha256"
 	echo
 
-	echo "Installing $1..."
+	echo "  - Installing $1..."
 	tar -C / -Jxpf "$1"
 }
+
+S6_ARCH=$(uname -m)
+echo "Detecting architecture: ${S6_ARCH}"
+
+S6_TEMP=$(mktemp -d)
+echo "Creating temporary directory: ${S6_TEMP}"
 
 cd "${S6_TEMP}" && {
 	s6_install "s6-overlay-noarch.tar.xz"
@@ -33,6 +37,6 @@ cd "${S6_TEMP}" && {
 	s6_install "syslogd-overlay-noarch.tar.xz"
 }
 
-echo "Cleaning up..."
+echo "Removing ${S6_TEMP}..."
 cd -
 rm -rf "${S6_TEMP}"
